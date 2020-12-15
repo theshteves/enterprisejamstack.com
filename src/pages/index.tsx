ace5505b2fbccaa91437fb0ee3f02670fa4c7c6f
Joel Hooks <joelhooks@gmail.com>
2020-12-14 17:47:49 -0800

    center it up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,8 +8,18 @@ export default function Home() {
         <title>Next.js, TypeScript, Tailwind, Jest</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <h1 className="text-3xl">Fast Effective Teams using Modern JAMStack🥞</h1>
+      <div className="sm:py-40 py-32 px-8 max-w-screen-md mx-auto min-h-screen flex flex-col items-center justify-center">
+        <div className="prose relative z-10 px-5 hide-toc">
+          <h1 className="text-3xl">
+            Fast Effective Teams using Modern JAMStack🥞
+          </h1>
+        </div>
+        <div className="bg-cool-gray-50 h-screen absolute z-0 top-0 w-full">
+          <div className="relative overflow-hidden w-full min-h-screen">
+            {/* <Background className="absolute left-0 top-0 w-screen h-screen overflow-hidden z-20" /> */}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
