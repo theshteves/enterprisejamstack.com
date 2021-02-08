78da65408a626cd86b7d08400f1c2e44ce417c9a
Vojta Holik <vojta@egghead.io>
2021-02-08 21:45:58 +0100

    fix image in safari

--- src/components/bio.tsx
+++ src/components/bio.tsx
@@ -4,13 +4,14 @@ import Image from 'next/image'
 const Bio = () => {
   return (
     <section className="max-w-screen-lg rounded-xl mx-auto md:bg-gray-900 flex items-center md:pt-8 md:flex-row flex-col">
-      <div className="flex-shrink-0 flex md:self-end sm:px-0 px-16">
-        <img
-          src="https://res.cloudinary.com/dg3gyk0gu/image/upload/v1612812906/jamstackwithjason.com/jason_2x.png"
-          width={380}
-          height={380}
-        />
-      </div>
+      <img
+        className="flex-shrink-0 flex md:self-end"
+        src="https://res.cloudinary.com/dg3gyk0gu/image/upload/v1612812906/jamstackwithjason.com/jason_2x.png"
+        width={380}
+        height={380}
+        alt="Jason dropping knowledge"
+      />
+
       <div className="p-8 md:pb-14 rounded-xl prose md:prose-lg bg-gray-900 sm:mx-0 mx-5">
         <h2>Lorem Ipsum Dolor</h2>
         <p>
