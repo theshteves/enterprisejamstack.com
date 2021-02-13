980a5962747cee7df21f93195f4a040a3e0904ea
Vojta Holik <vojta@egghead.io>
2021-02-13 23:40:56 +0100

    add aria-label

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <footer className="flex items-center justify-center lg:py-32 py-16">
       <div className="flex flex-col items-center justify-center space-y-2 transform scale-90">
         <Link href="/">
-          <a>
+          <a aria-label="Home">
             <Boxes className="sm:w-auto w-8" />
           </a>
         </Link>
