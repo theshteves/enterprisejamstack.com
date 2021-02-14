2ceae4ad95a95b08bf20091587637775cc98b5ce
Vojta Holik <vojta@egghead.io>
2021-02-14 18:24:37 +0100

    simplify bg

--- src/components/hero/background.tsx
+++ src/components/hero/background.tsx
@@ -1,55 +1,28 @@
 import * as React from 'react'
-import {motion} from 'framer-motion'
 import {random} from 'lodash'
-import {useInterval, useBoolean} from 'react-use'
 
 const Background = () => {
-  // restart animation every 8sec
-  const [count, setCount] = React.useState(0)
-  const [delay] = React.useState(8000)
-  const [isRunning] = useBoolean(true)
-  useInterval(
-    () => {
-      setCount(count + 1)
-    },
-    isRunning ? delay : null,
-  )
-
-  return isRunning ? (
+  return (
     <div className="overflow-hidden absolute top-0 left-0 w-full h-full">
-      <motion.div className="grid grid-cols-8 grid-rows-6 absolute top-0 left-0 h-screen overflow-hidden w-full ">
-        {new Array(8).fill({}).map((_, i) => {
-          const colStart = `col-start-${Math.floor(random(1, 8))}`
-          const colSpan = `col-span-${Math.floor(random(1, 4))}`
-          const rowStart = `row-start-${Math.floor(random(1, 6))}`
-          const rowSpan = `row-span-${Math.floor(random(1, 3))}`
-          const transformOrigin = i % 2 ? 'origin-left' : 'origin-right'
-
+      <div className="grid sm:grid-cols-6 grid-cols-3 grid-rows-4 absolute top-0 left-0 h-screen overflow-hidden w-full">
+        {new Array(24).fill({}).map((_, i) => {
+          const [opacity, setOpacity] = React.useState(random(20) % 2 ? 1 : 0)
+          React.useEffect(() => {
+            setOpacity(random(20) % 2 ? 1 : 0)
+          }, [])
           return (
-            <motion.div
-              key={`${colStart}-${colSpan}-${i}-${count}-${transformOrigin}`}
-              // animate={{scaleX: [0, 1]}}
-              // transition={{
-              //   type: 'ease-in-out',
-              //   duration: random(3, 8),
-              //   delay: 0.1 * i,
-              // }}
+            <div
               style={{
-                opacity: 0.6,
-                skewX: -16,
-                // scaleX: 0,
+                opacity: opacity,
                 zIndex: -i,
               }}
-              className={`
-               ${colStart} ${colSpan} ${rowStart} ${rowSpan} ${transformOrigin}
-                from-gray-1000 bg-gradient-to-t to-gray-900 relative overflow-hidden
-               `}
+              className=" from-gray-1000 bg-gradient-to-t to-gray-900 relative overflow-hidden"
             />
           )
         })}
-      </motion.div>
+      </div>
     </div>
-  ) : null
+  )
 }
 
 export default Background
